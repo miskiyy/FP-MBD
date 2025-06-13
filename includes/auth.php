a277fae7e8fd24dafb174fbaf86cdb467d897a6b
raflasr <162029447+raflasr@users.noreply.github.com>
2025-06-13 20:55:09 +0700

    Update auth.php

--- includes/auth.php
+++ includes/auth.php
@@ -1,7 +1,5 @@
-<?php
-session_start();
-if (!isset($_SESSION['user_id'])) {
-    header("Location: /public/login.php");
-    exit;
-}
-?>
+<footer class="text-center mt-5 text-muted">
+  <p>© 2025 CodingIn | Made with ❤️</p>
+</footer>
+</body>
+</html>
